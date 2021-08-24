30fab7539d92e6a230e7ba80b2053d0d55fcbf64
Jesús Mª Rico González <jrico@narranz.es>
2021-08-24 18:42:29 +0200

    Sc13: Herencia , Sc14: Poliformismo

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+class Comment {
+  constructor({
+    content,
+    studentName,
+    studentRole = "estudiante",
+  }) {
+    this.content = content;
+    this.studentName = studentName;
+    this.studentRole = studentRole;
+    this.likes = 0;
+  }
+
+  publicar() {
+    console.log(this.studentName + " (" + this.studentRole + ")");
+    console.log(this.likes + " likes");
+    console.log(this.content);
+  }
+}
+
+
 function videoPlay(id) {
   const urlSecreta = "https://platziultrasecretomasquelanasa.com/" + id;
   console.log("Se está reproduciendo desde la url " + urlSecreta);
@@ -121,6 +141,13 @@ class Student {
     this.approvedCourses = approvedCourses;
     this.learningPaths = learningPaths;
   }
+  publicarComentario(commentContent) {
+    const comment = new Comment({
+      content: commentContent,
+      studentName: this.name,
+    });
+    comment.publicar();
+  }
 }
 
 //! FREESTUDENT
@@ -163,6 +190,26 @@ class ExpertStudent extends Student {
   }
 }
 
+//! TEACHERSTUDENT 
+class TeacherStudent extends Student {
+  constructor(props) {
+    super(props);
+  }
+
+  approveCourse(newCourse) {
+    this.approvedCourses.push(newCourse);
+  }
+
+  publicarComentario(commentContent) {
+    const comment = new Comment({
+      content: commentContent,
+      studentName: this.name,
+      studentRole: "profesor",
+    });
+    comment.publicar();
+  }
+}
+
 const juan = new FreeStudent({
   name: "Alberto",
   username: "Alberto",
@@ -194,4 +241,11 @@ const jesus = new ExpertStudent({
     escuelaWeb,
     escuelaData,
   ],
+});
+
+const freddy = new TeacherStudent({
+  name: "Freddy Vega",
+  username: "freddier",
+  email: "f@gep.com",
+  instagram: "freddiervega",
 });
